e15292274dc7536af153b7025abdd1e7d215b6b4
mganopolsky <ganopolsky.marina@gmail.com>
2022-09-26 20:54:39 -0400

    first commit for DAY 3

---
+++ programs/myepicproject/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod myepicproject {
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+    Ok(())
+  }
+}
+
+#[derive(Accounts)]
+pub struct StartStuffOff {}
