84472e34e22ebabe46f9873a9073df6eac3dd45e
mganopolsky <ganopolsky.marina@gmail.com>
2022-09-27 17:13:12 -0400

    Day 4 part 2

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6NthZpToN6MSrbnSir1e8oYioV6Ko1cTSqPip6gXaYiz");
 
 #[program]
 pub mod myepicproject {
